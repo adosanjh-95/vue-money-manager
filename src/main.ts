df03c728d604c151afa8ccb540d89168767e5fec
amardeep <amardeep.dosanjh@and.digital>
2021-03-31 13:48:37 +0100

    Remove global stles in app and add extra icons needed

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,26 @@ import router from "./router";
 import store from "./store";
 import "@/assets/scss/main.scss";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faWallet, faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+import {
+  faWallet,
+  faBars,
+  faTimes,
+  faPlus,
+  faArrowCircleUp,
+  faArrowCircleDown,
+  faTrash,
+  faExpandAlt,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 library.add(faWallet);
 library.add(faBars);
 library.add(faTimes);
+library.add(faPlus);
+library.add(faArrowCircleUp);
+library.add(faArrowCircleDown);
+library.add(faTrash);
+library.add(faExpandAlt);
 
 const app = createApp(App);
 

010e6ac0ff6237d8e25bea6a90a0d9dc7186d86b
amardeep <amardeep.dosanjh@and.digital>
2021-03-31 13:50:23 +0100

    Initial home page with card icons for months. Add/delete buttons wired up

---
+++ src/components/MonthSummaryCard/MonthSummaryCard.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="month_card">
+    <h3 class="month_card__date">{{ getReadableDate }}</h3>
+    <h4 class="month_card__net">£{{ net.toFixed(2) }}</h4>
+    <div class="month_card__breakdown">
+      <h5 class="month_card__incoming">
+        <font-awesome-icon
+          :icon="['fas', 'arrow-circle-up']"
+          class="month_card__incoming_icon"
+        />
+        £{{ incoming.toFixed(2) }}
+      </h5>
+      <h5 class="month_card__outgoing">
+        <font-awesome-icon
+          :icon="['fas', 'arrow-circle-down']"
+          class="month_card__outgoing_icon"
+        />
+        £{{ outgoing.toFixed(2) }}
+      </h5>
+    </div>
+    <div class="month_card__actions">
+      <font-awesome-icon
+        :icon="['fas', 'trash']"
+        class="month_card__action month_card__action--trash"
+        @click="$emit('delete')"
+      />
+      <font-awesome-icon
+        :icon="['fas', 'expand-alt']"
+        class="month_card__action month_card__action--expand"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    date: { type: Date, required: true },
+    net: { type: Number, required: true },
+    incoming: { type: Number, required: true },
+    outgoing: { type: Number, required: true },
+  },
+  computed: {
+    getReadableDate(): string {
+      const monthNames = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ];
+      return `${monthNames[this.date.getMonth()]} ${this.date.getFullYear()}`;
+    },
+  },
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,42 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="toolbar">
+    <p class="toolbar__starting_amount">
+      Starting amount: <strong>£{{ startingAmount.toFixed(2) }}</strong>
+    </p>
+    <button class="new_month_btn" @click="addMonth">
+      <span class="new_month_btn__text">Add next month</span>
+      <font-awesome-icon :icon="['fas', 'plus']" class="new_month_btn__icon" />
+    </button>
+  </div>
+
+  <div class="months">
+    <MonthSummaryCard
+      v-for="month in monthsData"
+      :key="month.id"
+      v-bind="getMonthSummary(month.id)"
+      @delete="deleteMonth(month.id)"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { mapGetters, mapMutations, mapState } from "vuex";
+import MonthSummaryCard from "@/components/MonthSummaryCard/MonthSummaryCard.vue";
 
 export default defineComponent({
-  name: "Home",
+  computed: {
+    ...mapState(["startingAmount", "monthsData"]),
+    ...mapGetters(["getMonthSummary"]),
+  },
+  methods: {
+    ...mapMutations(["deleteMonth"]),
+    addMonth() {
+      this.$store.commit("addMonth");
+    },
+  },
   components: {
-    HelloWorld,
+    MonthSummaryCard,
   },
 });
 </script>
